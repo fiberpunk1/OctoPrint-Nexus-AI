e5eb34adce200f19021ed4373cc30a2183c78a65
jneilliii <jneilliii+github@gmail.com>
2022-09-23 01:13:29 -0400

    remove numpy dependency

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/fiberpunk1/OctoPrint-Nexus-AI"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["numpy==1.21.4"]
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
